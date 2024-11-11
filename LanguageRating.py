201adf9bfbdd152a005e372f6db51ee6a7a61950
Dominik Storck <dominik.storck@tum.de>
2024-11-10 21:34:28 -0500

    Small adjustments

--- LanguageRating.py
+++ LanguageRating.py
@@ -1,5 +1,7 @@
+import nltk
 from nltk import FreqDist, ngrams, word_tokenize
 import requests
+from collections import Counter
 
 #if not already installed
 #nltk.download('punkt')  # For tokenizing
@@ -32,7 +34,23 @@ def calculate_ngram_frequencies(text, n):
 # Frequency distributions for French
 unigram_freq = calculate_ngram_frequencies(text, 1)
 bigram_freq = calculate_ngram_frequencies(text, 2)
-trigram_freq = calculate_ngram_frequencies(text, 3)
+#trigram_freq = calculate_ngram_frequencies(text, 3)
+
+# Step 2.1: Include character trigrams:
+def build_trigram_frequency(text):
+    text.lower()
+    # Generate trigrams
+    trigrams = [text[i:i+3] for i in range(len(text) - 2)]
+    #Count frequency of each trigram
+    trigram_freq = Counter(trigrams)
+    # Sort by frequency in descending order
+    common_trigrams = {item: count for item, count in trigram_freq.items() if count > 5}#trigram_freq.most_common()
+    #print(common_trigrams)
+    return common_trigrams
+
+    return score
+
+trigram_freq = build_trigram_frequency(text)
 
 # Step 3: Define the scoring function
 def rate_decrypted_text(decrypted_text):
@@ -59,10 +77,16 @@ def rate_decrypted_text(decrypted_text):
             score -= 2  # Penalize for uncommon bigrams
 
     # Score based on trigram frequencies
-    for trigram in trigrams:
+    #for trigram in trigrams:
+        #if trigram in trigram_freq:
+            #score += trigram_freq[trigram] * 3  # Weight trigrams more heavily
+        #else:
+         #   score -= 3  # Penalize for uncommon trigrams
+
+    #Calculate score based on trigram frequency in the decrypted text
+    for i in range(len(decrypted_text) - 2):
+        trigram = decrypted_text[i:i + 3]
         if trigram in trigram_freq:
-            score += trigram_freq[trigram] * 3  # Weight trigrams more heavily
-        else:
-            score -= 3  # Penalize for uncommon trigrams
+            score += trigram_freq[trigram]
 
     return score
